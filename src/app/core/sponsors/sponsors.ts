e75971dc5066a6c5354fc8d0da128d297875d337
Lamela <lameladev@gmail.com>
2025-10-26 14:14:21 +0100

    sponsor logos to webp

--- src/app/core/sponsors/sponsors.ts
+++ src/app/core/sponsors/sponsors.ts
@@ -7,21 +7,21 @@ import { Component, Input } from '@angular/core';
 })
 export class SponsorsComponent {
   @Input() principalSponsors: { name: string; logo: string }[] = [
-    { name: 'Sponsor 1', logo: 'assets/images/sponsors/logo-ferrol.png' },
-    { name: 'Sponsor 2', logo: 'assets/images/sponsors/logo-cardmarket.png' },
+    { name: 'Sponsor 1', logo: 'assets/images/sponsors/logo-ferrol.webp' },
+    { name: 'Sponsor 2', logo: 'assets/images/sponsors/logo-cardmarket.webp' },
   ];
   @Input() secondarySponsors: { name: string; logo: string }[] = [
     {
       name: 'Sponsor 1',
-      logo: 'assets/images/sponsors/logo-random-player.png',
+      logo: 'assets/images/sponsors/logo-random-player.webp',
     },
-    { name: 'Sponsor 2', logo: 'assets/images/sponsors/logo-vip.png' },
-    { name: 'Sponsor 3', logo: 'assets/images/sponsors/logo-xpoly.png' },
+    { name: 'Sponsor 2', logo: 'assets/images/sponsors/logo-vip.webp' },
+    { name: 'Sponsor 3', logo: 'assets/images/sponsors/logo-xpoly.webp' },
 
     {
       name: 'Sponsor 4',
-      logo: 'assets/images/sponsors/logo-caprica.png',
+      logo: 'assets/images/sponsors/logo-caprica.webp',
     },
-    { name: 'Sponsor 5', logo: 'assets/images/sponsors/logo-percalandia.png' },
+    { name: 'Sponsor 5', logo: 'assets/images/sponsors/logo-percalandia.webp' },
   ];
 }
